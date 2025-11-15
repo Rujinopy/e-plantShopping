2b48712e7c04ccfa77013025a8f8bb67a1fdafcc
Rujinopy <ruth3442@gmail.com>
2025-11-16 01:18:17 +0700

    edit cart and products grid

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -8,11 +8,29 @@ export const CartSlice = createSlice({
   reducers: {
     addItem: (state, action) => {
     
+      const productToAdd = action.payload; // product is passed in action.payload
+      const existingItem = state.items.find(item => item.name === productToAdd.name);
+      if (existingItem) {
+        existingItem.quantity += 1; // if item already exists then increase quantity by 1
+      } else {
+        state.items.push({ ...productToAdd, quantity: 1 }); // if item doesn't exist then add new item with quantity 1
+      }
     },
     removeItem: (state, action) => {
+      const productIdToRemove = action.payload;
+      state.items = state.items.filter(item => item.name !== productIdToRemove.name);
     },
     updateQuantity: (state, action) => {
+      const { productName, quantity } = action.payload;
+      const itemToUpdate = state.items.find( item => item.name === productName);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
 
+      if (itemToUpdate.quantity <= 0) {
+        state.items = state.items.filter(item => item.name !== productName);
+
+      }
     
     },
   },
